b986cc12a869974250627cd166dac7963f9e4a1b
Kevin <dev@northkevin.com>
2019-07-13 23:57:50 -0700

    increasing log to debug
    modifying buildpack from hexdocs.pm

--- config/prod.exs
+++ config/prod.exs
@@ -22,7 +22,7 @@ config :chat, ChatWeb.Endpoint,
   secret_key_base: Map.fetch!(System.get_env(), "SECRET_KEY_BASE")
 
 # Do not print debug messages in production
-config :logger, level: :info
+config :logger, level: :debug
 
 # Configure your database
 config :chat, Chat.Repo,
